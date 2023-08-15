2103cc46e1670cc452ea123354c4b3ea02c6202a
uladzislaupapou <vladcrwrpopov@gmail.com>
2023-08-15 17:00:14 +0200

    sections header, hero, popular, footer are completed

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-$(function () {})
+$(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1) {
+			$('.header').addClass('sticky')
+		} else {
+			$('.header').removeClass('sticky')
+		}
+	})
+})
