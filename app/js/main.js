a6fd2a211c30905e7d7b05653727883a93cea1a5
uladzislaupapou <vladcrwrpopov@gmail.com>
2023-09-09 11:10:25 +0200

    catalog page completed

--- app/js/main.js
+++ app/js/main.js
@@ -4,19 +4,31 @@ document.addEventListener('DOMContentLoaded', () => {
 	const mobileMenu = document.querySelector('.mobile-menu')
 	const bodyLock = document.querySelector('body')
 	const headerElement = document.querySelector('.header')
+	const filterMenu = document.querySelector('.filter')
+	const filterBtn = document.querySelector('.filter-btn')
+	const filterCloseBtn = document.querySelector('.filter__btn-close')
 
-	function addMobileMenu() {
-		mobileMenu.classList.add('mobile-menu--active')
+	function addLock() {
 		bodyLock.classList.add('lock')
 		headerElement.classList.add('header-lock')
 	}
-  function removeMobileMenu() {
-		mobileMenu.classList.remove('mobile-menu--active')
+
+	function removeLock() {
 		bodyLock.classList.remove('lock')
 		headerElement.classList.remove('header-lock')
 	}
 
-  burger.addEventListener('click', () => {
+	function addMobileMenu() {
+		mobileMenu.classList.add('mobile-menu--active')
+		addLock()
+	}
+
+	function removeMobileMenu() {
+		mobileMenu.classList.remove('mobile-menu--active')
+		removeLock()
+	}
+
+	burger.addEventListener('click', () => {
 		addMobileMenu()
 	})
 
@@ -24,18 +36,97 @@ document.addEventListener('DOMContentLoaded', () => {
 		removeMobileMenu()
 	})
 
+	filterBtn.addEventListener('click', () => {
+		filterMenu.classList.add('filter--active')
+		filterCloseBtn.classList.add('filter__btn-close--active')
+		addLock()
+	})
+
+	filterCloseBtn.addEventListener('click', () => {
+		filterMenu.classList.remove('filter--active')
+		filterCloseBtn.classList.remove('filter__btn-close--active')
+		removeLock()
+	})
+
 	document.addEventListener('click', function (e) {
 		if (
 			e.target !== mobileMenu &&
 			!mobileMenu.contains(e.target) &&
-			e.target !== burger
+			e.target !== burger &&
+			e.target !== filterMenu &&
+			!filterMenu.contains(e.target) &&
+			e.target !== filterBtn
 		) {
+			filterMenu.classList.remove('filter--active')
+			filterCloseBtn.classList.remove('filter__btn-close--active')
 			removeMobileMenu()
 		}
 	})
 })
 
 $(function () {
+	var $range = $('.filter-price__input')
+	var $inputFrom = $('.filter-price__from')
+	var $inputTo = $('.filter-price__to')
+	var instance
+	var min = 0
+	var max = 1000
+	var from = 0
+	var to = 0
+
+	$range.ionRangeSlider({
+		type: 'double',
+		min: min,
+		max: max,
+
+		onStart: updateInputs,
+		onChange: updateInputs,
+		onFinish: updateInputs,
+	})
+	instance = $range.data('ionRangeSlider')
+
+	function updateInputs(data) {
+		from = data.from
+		to = data.to
+
+		$inputFrom.prop('value', from)
+		$inputTo.prop('value', to)
+	}
+
+	$inputFrom.on('change', function () {
+		var val = $(this).prop('value')
+
+		// validate
+		if (val < min) {
+			val = min
+		} else if (val > to) {
+			val = to
+		}
+
+		instance.update({
+			from: val,
+		})
+
+		$(this).prop('value', val)
+	})
+
+	$inputTo.on('change', function () {
+		var val = $(this).prop('value')
+
+		// validate
+		if (val < from) {
+			val = from
+		} else if (val > max) {
+			val = max
+		}
+
+		instance.update({
+			to: val,
+		})
+
+		$(this).prop('value', val)
+	})
+
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 1) {
 			$('.header').addClass('sticky')
@@ -55,6 +146,22 @@ $(function () {
 				.removeClass('menu-list__link--active')
 			$(this).addClass('menu-list__link--active')
 		})
+
+	$(window).on('load resize', function () {
+		if ($(window).width() < 768) {
+			$('.promo__items:not(.slick-initialized)').slick({
+				arrows: false,
+				dots: true,
+				infinite: true,
+				speed: 500,
+				slidesToShow: 1,
+				autoplay: true,
+				autoplaySpeed: 3000,
+			})
+		} else {
+			$('.promo__items.slick-initialized').slick('unslick')
+		}
+	})
 
 	$(window).on('load resize', function () {
 		if ($(window).width() < 768) {
@@ -92,6 +199,21 @@ $(function () {
 				},
 			},
 		],
+	})
+
+	$('.select-style').styler()
+
+	$('.scroll').on('click', function (event) {
+		event.preventDefault()
+		var id = $(this).attr('href'),
+			top = $(id).offset().top
+		$('body,html').animate({ scrollTop: top }, 1500)
+	})
+
+	$('.menu-list__link').on('click', function () {
+		$('.mobile-menu').removeClass('mobile-menu--active')
+		$('.header').removeClass('header-lock')
+		$('body').removeClass('lock')
 	})
 })
 
--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,9 @@ function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/mixitup/dist/mixitup.js',
-    'node_modules/slick-carousel/slick/slick.js',
+		'node_modules/slick-carousel/slick/slick.js',
+		'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+		'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
 		'app/js/main.js',
 	])
 		.pipe(concat('main.min.js'))
